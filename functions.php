bf5d719b08d7088df88aeadfc7c99cc5ceee9050
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2026-04-29 14:51:30 +0200

    Update stylesheet version from 1.2.0 to 1.2.1
    version bump

--- functions.php
+++ functions.php
@@ -27,7 +27,7 @@ function aestazia_child_enqueue_styles() {
 		'aestazia-child',
 		get_stylesheet_uri(),
 		array( 'aestazia-parent' ),
-		'1.2.0'
+		'1.2.1'
 	);
 }
 add_action( 'wp_enqueue_scripts', 'aestazia_child_enqueue_styles' );
